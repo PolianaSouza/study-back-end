4512e023e63f06b284cfb03b4bb715ffda8837fc
poliana.souza <polysantos88@gmail.com>
2023-04-18 10:39:52 -0300

    Inclusão dos tratamentos de erros

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +10,9 @@ async function bootstrap() {
       whitelist: true, //defino uma lista = DTO
       forbidNonWhitelisted: true, //recusa algo fora do white list
       transform: true, //transforma os dados de acordo com o DTO
-    })
-  )
-  await app.listen(process.env.PORT ||3000);
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
