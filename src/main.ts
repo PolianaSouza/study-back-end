af13da031f1d0d0e4d44ad0b7124ef7b270ecfed
poliana.souza <polysantos88@gmail.com>
2023-04-18 12:01:13 -0300

    Inclusão dos interceptadores de erro

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +14,9 @@ async function bootstrap() {
       transform: true, //transforma os dados de acordo com o DTO
     }),
   );
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { StudentsRepository } from './repositories/students.repository';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
+import { StudentEntity } from './entities/student.entity';
 
 @Injectable()
 export class StudentsService {
-
   constructor(private readonly repository: StudentsRepository) {}
 
   create(createStudentDto: CreateStudentDto) {
@@ -13,11 +14,18 @@ export class StudentsService {
   }
 
   findAll() {
+    // throw new UnauthorizedError('Não autorizado');
     return this.repository.findAll();
   }
 
-  findOne(id: string) {
-    return this.repository.findOne(id);
+  async findOne(id: string): Promise<StudentEntity> {
+    const user = await this.repository.findOne(id);
+
+    if (!user) {
+      throw new NotFoundError('Estudante não encontrado!');
+    }
+
+    return user;
   }
 
   update(id: string, updateStudentDto: UpdateStudentDto) {
